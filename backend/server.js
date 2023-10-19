4eec504ff06c45e5b09ef95397fe9f31314c7043
lakhan368 <cloud_user@ac189877731c.mylabserver.com>
2023-10-19 21:05:07 +0000

    Completing 14 and 15

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import products from "./data/products.js"
-const port = 5000;
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT || 8080; 
+
 const app = express();
 
 app.get('/',(req, res) => {
@@ -11,4 +14,11 @@ app.get('/api/products',(req, res) => {
     res.json(products);
     });
 
-app.listen(port, () => console.log(`server running on port ${port}`))
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+    });
+
+
+app.listen(port, () => console.log(`server running on port ${port}`));
