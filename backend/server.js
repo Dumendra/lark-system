60f620094cee149e5357b4bb35cf5bf51e78fa6c
lakhan368 <lakhan368@outlook.com>
2023-11-11 06:56:37 +0530

    26 Product controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 8080; 
 
 
@@ -13,7 +14,11 @@ app.get('/',(req, res) => {
     res.send("API is running");
 });
 
-app.use("/api/products", productRoutes);
+
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(port, () => console.log(`server running on port ${port}`));
